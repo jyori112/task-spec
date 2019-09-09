ae4c24c2016d3b510b9119645b14c780b45dd83f
jyori112 <jyori112@gmail.com>
2019-09-09 22:27:33 +0900

    Add probability in prediction

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 import sys
 import json
 from pathlib import Path
+import logging
+import gc
 
 import click
 import numpy as np
@@ -10,24 +12,9 @@ from data import Vocab, Dataset
 from model import MultiClassModel
 from config import Config
 
-def compute_accuracy(x, y):
-    return torch.mean((x == y).float())
-
-def evaluate(model, eval_set, device):
-    model.eval()
-    accuracies = []
-    losses = []
-
-    for labels, tokens, mask in eval_set.batches():
-        labels = labels.to(device)
-        tokens = tokens.to(device)
-        mask = mask.to(device)
-        result = model(tokens, mask, label=labels, predict=True)
-        losses.append(float(result['loss']))
-        accuracy = compute_accuracy(result['pred'], labels)
-        accuracies.append(float(accuracy))
-
-    return np.mean(losses), np.mean(accuracies)
+logger = logging.getLogger(__name__)
+LOG_FORMAT = '[%(asctime)s] [%(levelname)s] %(message)s (%(funcName)s@%(filename)s:%(lineno)s)'
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
 def batch_pad(batch_tokens, vocab):
     max_len = max(len(tokens) for tokens in batch_tokens)
@@ -51,9 +38,11 @@ def main(model_dir, ckpt, device, emb_path):
         vocab = Vocab.load(fwords, flabels)
 
     model = MultiClassModel(vocab, config.model)
+    logger.info("Loading parameters")
     model.load_state_dict(torch.load(model_dir / ckpt))
 
     if emb_path:
+        logger.info("Loading embeddings")
         with open(emb_path) as femb, open(config.vocab.labels) as flabels:
             vocab, vecs = Vocab.build_from_emb(femb, flabels)
 
@@ -66,6 +55,9 @@ def main(model_dir, ckpt, device, emb_path):
 
     model = model.to(device)
 
+    model.eval()
+
+    logger.info("Start prediction")
     input_lines = []
     batch_tokens = []
     for i, line in enumerate(sys.stdin):
@@ -76,22 +68,33 @@ def main(model_dir, ckpt, device, emb_path):
 
         if (i + 1) % 32 == 0:
             batch_padded_tokens, batch_mask = batch_pad(batch_tokens, vocab)
-            batch_padded_tokens = torch.tensor(batch_padded_tokens, dtype=torch.long).to(device)
-            batch_mask = torch.tensor(batch_mask).to(device)
+            batch_padded_tokens = torch.tensor(batch_padded_tokens, dtype=torch.long, requires_grad=False).to(device)
+            batch_mask = torch.tensor(batch_mask, requires_grad=False).to(device)
             result = model(batch_padded_tokens, batch_mask, predict=True)
+
+            prob = torch.nn.functional.softmax(result['logits'], dim=1)
 
             for i in range(len(result['pred'])):
                 print(json.dumps({
                     'input': input_lines[i], 
+                    'prob': {vocab.id2label(j): float(prob[i,j]) for j in range(prob.shape[1])},
                     'prediction': vocab.id2label(int(result['pred'][i]))
                 }))
+            input_lines = []
+            batch_tokens = []
+
+    if not input_lines:
+        return
 
     batch_padded_tokens, batch_mask = batch_pad(batch_tokens, vocab)
     result = model(batch_padded_tokens, batch_mask, predict=True)
 
+    prob = torch.nn.functional.softmax(result['logits'], dim=1)
+
     for i in len(result['pred']):
         print(json.dumps({
             'input': input_lines[i], 
+            'prob': {vocab.id2label(j): float(prob[i,j]) for j in range(prob.shape[1])},
             'prediction': vocab.id2label(int(result['pred'][i]))
         }))
 
